f66349042ece37118a2a83a3eedef2c30c256834
EyuReaper <eyureaper@gmail.com>
2024-11-12 01:40:03 +0300

    learned about loops

---
+++ loops.py
@@ -0,0 +1,35 @@
+# a loop is used for iterating over a sequence of either (a list, a tuple, a dictionary, a string, a range)
+
+people = ['john', 'paul', 'george', 'ringo']
+
+#simple for loop
+#for person in people:
+ #   print(f'current person: {person}')
+
+#break
+#for person in people:
+ #   if person == 'george':
+ #       break
+  #  print(f'current person: {person}')
+
+#continue
+#for person in people:
+#    if person == 'george':
+#        continue
+#    print(f'current person: {person}')
+
+#range
+#for i in range(len(people)):
+#    print(people[i])
+
+#for i in range (0,11):
+#    print(f'Number: {i}')
+
+#while loop excute a set of statement as long as the condition is true.
+
+count = 0
+while count < 10:
+    print(f'count: {count}')
+    count += 1 
+
+#while loops excute a set of statements as long as a condition is true
